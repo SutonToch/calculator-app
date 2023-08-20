1dbe3504f5318fe52fb696499fdcd9c1de07f487
SutonToch <77286229+SutonToch@users.noreply.github.com>
2023-08-20 20:49:47 +0200

    ADD keyboard presses work too

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ const delButton = $("#del-btn");
 const resetButton = $("#reset-btn");
 const equalButton = $(".equal-btn");
 let firstInput = true;
+let lastKeypressTime = 0;
 
+// THEME-BUTTON EVENT LISTENER
 radio1.click(() => {
     $(body).removeClass();
     $(body).addClass("theme1");
@@ -24,16 +26,14 @@ radio3.click(() => {
     $(body).addClass("theme3");
 })
 
+
+// KEYPAD EVENT LISTENER
 inputButtons.click((e) => {
     addToDisplay(e.currentTarget.textContent)
 });
 
 delButton.click(() => {
-    if(display.textContent.length == 1) {
-        resetDisplay();
-        return;
-    }
-    display.textContent = display.textContent.slice(0, -1);
+    deleteLastCharFromDisplay();
 });
 
 resetButton.click(() => {
@@ -47,7 +47,58 @@ equalButton.click(() => {
     evaluateDisplay();
 });
 
-
+// KEYPAD WITH KEYBOARD
+$(document).on("keydown", function(e) {
+    e.preventDefault();
+    if(e.keyCode == 13 || (e.shiftKey && e.keyCode == 48)) {
+        if(firstInput) {
+            return;
+        }
+        evaluateDisplay();
+    } else if(e.keyCode == 32) {
+        //reset on double spacebar
+        const delta = 500;
+        let thisKeypressTime = new Date();
+        
+        if(thisKeypressTime - lastKeypressTime <= delta) {
+          resetDisplay();
+          thisKeypressTime = 0;
+        }
+        lastKeypressTime = thisKeypressTime;
+    } else if(e.keyCode == 46 || e.keyCode == 8) {
+        deleteLastCharFromDisplay()
+    } else if(e.keyCode == 111 || (e.shiftKey && e.keyCode == 55)) {
+        addToDisplay("/");
+    } else if(e.keyCode == 88 || e.keyCode == 106 || (e.shiftKey && e.keyCode == 171)) {
+        addToDisplay("x");
+    } else if(e.keyCode == 173 || e.keyCode == 109) {
+        addToDisplay("-");
+    } else if(e.keyCode == 171 || e.keyCode == 107) {
+        addToDisplay("+");
+    } else if(e.keyCode == 48 || e.keyCode == 96) {
+        addToDisplay("0");
+    } else if(e.keyCode == 49 || e.keyCode == 97) {
+        addToDisplay("1");
+    } else if(e.keyCode == 50 || e.keyCode == 98) {
+        addToDisplay("2");
+    } else if(e.keyCode == 51 || e.keyCode == 99) {
+        addToDisplay("3");
+    } else if(e.keyCode == 52 || e.keyCode == 100) {
+        addToDisplay("4");
+    } else if(e.keyCode == 53 || e.keyCode == 101) {
+        addToDisplay("5");
+    } else if(e.keyCode == 54 || e.keyCode == 102) {
+        addToDisplay("6");
+    } else if(e.keyCode == 55 || e.keyCode == 103) {
+        addToDisplay("7");
+    } else if(e.keyCode == 56 || e.keyCode == 104) {
+        addToDisplay("8");
+    } else if(e.keyCode == 57 || e.keyCode == 105) {
+        addToDisplay("9");
+    } else if(e.keyCode == 190 || e.keyCode == 188 || e.keyCode == 110) {
+        addToDisplay(".");
+    }
+});
 
 function addToDisplay(input) {
     const incorrectInput = checkForIncorrectInput(input);
@@ -87,6 +138,15 @@ function checkForIncorrectInput(input) {
         }
     }
 }
+
+function deleteLastCharFromDisplay() {
+    if(display.textContent.length == 1) {
+        resetDisplay();
+        return;
+    }
+    display.textContent = display.textContent.slice(0, -1);
+}
+
 
 function resetDisplay() {
     display.textContent = "0";
